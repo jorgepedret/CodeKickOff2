535274e08715a4c076d5a55576e6fa0c587fb56c
jorgepedret <jorgepedret@gmail.com>
2011-07-09 01:37:13 -0700

    Forgot to add the footer...

---
+++ footer.php
@@ -0,0 +1,51 @@
+		</div>
+	</article>
+	<footer id="footer">
+		<h3>Designed &amp; Built by <a href="http://www.websimians.com/">Web Simians</a></h3>
+	</footer>
+</div> <!-- end of #wrap -->
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.js"></script>
+<script>!window.jQuery && document.write(unescape('%3Cscript src="<?php bloginfo('stylesheet_directory');?>/js/libs/jquery-1.4.2.js"%3E%3C/script%3E'))</script>
+<!-- scripts concatenated and minified via ant build script-->
+<script src="<?php bloginfo('stylesheet_directory');?>/js/plugins.js?v=0.2"></script>
+<script src="<?php bloginfo('stylesheet_directory');?>/js/script.js?v=0.2"></script>
+<!-- end concatenated and minified scripts-->
+<!--[if lt IE 7 ]>
+<script src="<?php bloginfo('stylesheet_directory');?>/js/libs/dd_belatedpng.js"></script>
+<script> DD_belatedPNG.fix('img, .png_bg'); </script>
+<![endif]-->
+<a href="https://github.com/jorgepedret/CodeKickOff2"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://gs1.wac.edgecastcdn.net/80460E/assets/img/e6bef7a091f5f3138b8cd40bc3e114258dd68ddf/687474703a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f7265645f6161303030302e706e67" alt="Fork me on GitHub"></a>
+<?php wp_footer(); ?>
+<script type="text/javascript">
+  var uservoiceOptions = {
+    key: 'codekickoff',
+    host: 'codekickoff.uservoice.com', 
+    forum: '102393',
+    alignment: 'left',
+    background_color:'#ff6b51', 
+    text_color: 'white',
+    hover_color: '#0066CC',
+    lang: 'en',
+    showTab: true
+  };
+  function _loadUserVoice() {
+    var s = document.createElement('script');
+    s.src = ("https:" == document.location.protocol ? "https://" : "http://") + "cdn.uservoice.com/javascripts/widgets/tab.js";
+    document.getElementsByTagName('head')[0].appendChild(s);
+  }
+  _loadSuper = window.onload;
+  window.onload = (typeof window.onload != 'function') ? _loadUserVoice : function() { _loadSuper(); _loadUserVoice(); };
+</script>
+<!-- change the UA-XXXXX-X to be your site's ID -->
+<script>
+var _gaq = [['_setAccount', 'UA-11181880-20'], ['_trackPageview']];
+(function(d, t) {
+var g = d.createElement(t),
+s = d.getElementsByTagName(t)[0];
+g.async = true;
+g.src = ('https:' == location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+s.parentNode.insertBefore(g, s);
+})(document, 'script');
+</script>
+</body>
+</html>
